b6e64969b88108f986fc6bad91a11c8560b5db3d
lamberete <74853880+lamberete@users.noreply.github.com>
2021-01-16 13:54:12 +0100

    ammend for latest dle.rae.es/data changes
    - dle.rae.es/data has updated its api, it no longer support the &t parameter (or ignores it) and the maximum number of results is 40 (so less than 40 groups if any gruop has more than 1 result)
    - wget subprocess replaced with native python http requests
    - internal methods are now private
    - added some setter methods
    - user, password and output files are now moved to command line arguments (type lemario_extractor.py -h to see them)
    - some minor cosmetic amends

--- lemario_extractor.py
+++ lemario_extractor.py
@@ -1,33 +1,43 @@
-33
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 from enum import Enum
-import sys
-import subprocess
+import argparse
 import json
 import re
+import requests
+import sys
+import time
 
 class Mode(Enum):
   PREFIX  = "31"
   SUFIX   = "32"
   INFIX   = "33"
 
-user = "p682JghS3"
-password = "aGfUdCiE434"
-
 class LemarioExtractor:
-  def __init__ (self, alphabet, mode = Mode.PREFIX, initialSearch = None):
+  # constructor: mandatory alphabet and optional mode and initial search term
+  def __init__ (self, alphabet, mode = Mode.PREFIX, initialSearch = None, stopSearch = None):
     self.alphabet = alphabet
     self.mode = mode
     self.search = initialSearch if initialSearch != None else alphabet[0]
+    self.stop = stopSearch
+
+  # overwrite current alphabet and initial search term
+  def setAlphabet(self, alphabet, initialSearch = None, stopSearch = None):
+    self.alphabet = alphabet
+    self.search = initialSearch if initialSearch != None else alphabet[0]
+    self.stop = stopSearch
+
+  # overwrite current mode
+  def setMode(self, mode):
+    self.mode = mode
 
   # updates search term to alphabetically next longer one
-  def longerPrefix(self):
+  def __longerSearch(self):
     self.search += self.alphabet[0]
 
   # updates search term to alphabetically next same lenght or shorter one
-  def nextPrefix(self):
+  def __nextSearch(self):
     if (len(self.search) == 0):
       # nothing to do for an empty search term
       pass
@@ -35,63 +45,103 @@ class LemarioExtractor:
       # search does not end with last letter of alphabet, increase it
       nextLetter = self.alphabet[self.alphabet.find(self.search[-1]) + 1]
       self.search = self.search[0:-1] + nextLetter
+    # TODO: consider doing this in a fancier way, maybe in a dedicated method
     elif len(self.search) > 2 and self.search[-2] == " ":
       # search term ends with last letter of alphabet but also the previous one happens to be an space, 
       # remove both, call nextPrefix recursively and add back the space if needed
-      # TODO: consider doing this in a fancier way, maybe in a dedicated method
       self.search = self.search[0:-2]
-      self.nextPrefix()
+      self.__nextSearch()
       if len(self.search) > 0:
         # now add the space and the first letter
         self.search += " " + self.alphabet[0]
     else:
       # search term ends with the last letter of the alphabet, remove it and call nextPrefix recursively
       self.search = self.search[0:-1]
-      self.nextPrefix()
+      self.__nextSearch()
 
   # fetches rae result for words with current search and mode
-  def getRaeWords(self):
+  def __getRaeLemasApp(self, user, password):
+    maxResults = 40
     maxGroups = 200
-    # TODO: use natively with urllib
-    result = subprocess.run(["wget", "-O", "-", "-o", "/dev/null", "https://" + user + ":" + password + "@dle.rae.es/data/search?m=" + self.mode.value + "\&t=1000\&w=" + self.search], stdout=subprocess.PIPE)
-    jsonResult = json.loads(result.stdout)['res']
-    lastGroup = int(jsonResult[-1]['grp'])+1 if len(jsonResult)>0 else 0
-    print("{0}: {1}".format(self.search, lastGroup), file=sys.stderr, flush=True)
-    return jsonResult, lastGroup >= maxGroups
+    url = "https://dle.rae.es/data/search?w={0}&m={1}".format(self.search, self.mode.value, user, password)
+    numErrors = 0
+    while True:
+      try:
+        result = requests.get(url, auth=(user, password))
+        jsonResults = json.loads(result.text)['res']
+        break;
+      except:
+        numErrors += 1
+        #print("Error parsing response: {0}".format(result.text), file=sys.stderr, flush=True)
+        if numErrors < 10:
+          time.sleep(1)
+        else:
+          sys.exit()
+    numResults = len(jsonResults)
+    lastGroup = int(jsonResults[-1]['grp'])+1 if len(jsonResults)>0 else 0
+    # convert results into lema-id pair
+    lemas = []
+    #regex to return the lema, ignoring <i> markup and removing <sup> ones
+    p = re.compile('(?:<i>)?([^<.]*)\.?(?:</i>)?(?:<sup>.*</sup>)?([^<.]*)\.?') 
+    for res in jsonResults:
+      # remove markups from header
+      m = p.match(res['header'])
+      lema = m.group(1) + m.group(2)
+      # compose simple lema entry
+      lemas.append({"lema": lema, "id": res['id']})
 
-  # prints lema and id in a csv format
-  def printRaeWord(self, word):
-    # remove markups from header
-    p = re.compile('(<i>)?([^<.]*)\.?(</i>)?(<sup>.*</sup>)?([^<.]*).') #regex to return the lema, ignoring <i> markup and removing <sup> ones
-    m = p.match(word['header'])
-    lema = m.group(2) + m.group(5)
-    # print lema and id
-    print("\"{0}\", \"{1}\"".format(lema, word['id'], flush = True))
+    # log this search result
+    print("{0}: {1} - {2} - ({3})".format(self.search, numResults, result.elapsed, numErrors), file=searches, flush=True)
+    return lemas, numResults >= maxResults
+
+  # prints given lemas and ids in a csv format
+  def __printRaeLemas(self, lemas):
+    for lema in lemas:
+      print("\"{0}\", \"{1}\"".format(lema['lema'], lema['id']), file=lemario)
 
   # extracts alphabetically rae lemario starting with a given prefix
   def getLemario(self):
     # loop until no search term
-    while len(self.search) > 0:
-      words, max = self.getRaeWords()
+    while len(self.search) > 0 and self.search != self.stop:
+      lemas, max = self.__getRaeLemasApp(user, password)
       # send founded words (if any) to the output
-      for word in words:
-        self.printRaeWord(word)
-      if max:
-        # too many groups for this prefix, try with a longer one
-        # duplicates will be removed in post processing
-        self.longerPrefix()
-      else:
-        self.nextPrefix()
+      self.__printRaeLemas(lemas)
       # TODO: Add a warning/abort condition if search term exceeds a set lenght
+      if max:
+        # too many groups for this searchTerm, try with a longer one
+        # (duplicates will be removed in post processing)
+        self.__longerSearch()
+      else:
+        self.__nextSearch()
+
 
 # lets rock'n'roll
 def main():
+  # Get lowercase standard lemas
   LemarioExtractor("aábcdeéfghiíjklmnñoópqrstuúüvwxyz-").getLemario()
+  # Get lemas that include capital letters
   LemarioExtractor("AÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ", Mode.INFIX).getLemario()
+  # Get lemas that include spaces
   LemarioExtractor("aábcdeéfghiíjklmnñoópqrstuúüvwxyz", Mode.INFIX, "a a").getLemario()
-  LemarioExtractor("èî‒", Mode.INFIX).getLemario()
+  # Get lemas that include rare chars
+  LemarioExtractor("àèìòùâêîôûäëïö‒", Mode.INFIX).getLemario()
 
 
 if __name__ == "__main__":
   # execute only if run as a script
+  parser = argparse.ArgumentParser(description='Searches dle.rae.es for existing lemas')
+  parser.add_argument('user', help='username to connect to dle.rae.es')
+  parser.add_argument('password', help='password to connect to dle.rae.es')
+  parser.add_argument('-l', '--lemario', metavar='FILE', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='file to append lemas founded, defaults to <stdin>')
+  parser.add_argument('-s', '--searches', metavar='FILE', nargs='?', type=argparse.FileType('w'), default=sys.stderr, help='file to append searches made, defaults to <stderr>')
+  #TODO: add alphabet, mode, initialSearch and stopSearch to command line, skip main 
+
+  args = parser.parse_args()
+
+  #TODO: convert this global vars in main method parameters and LemarioExtractor attributes
+  user = args.user
+  password = args.password
+  lemario = args.lemario
+  searches = args.searches
+
   main()
